478fa5950996fb9673bdea5fc8aba4862743b5bb
empathicqubit <empathicqubit@entan.gl>
2021-05-01 22:04:23 -0400

    Fix Xpra script

--- xpra-run.sh
+++ xpra-run.sh
@@ -5,7 +5,7 @@ ARGS=()
 PORT=5309
 xpra start --bind-tcp=127.0.0.1:$PORT --html=on \
     --start-child="/etc/alternatives/x-terminal-emulator -e 'lsnes ${ARGS[*]}'" \
-    --exit-with-child=yes --start-new-commands=no
+    --exit-with-child=yes --start-new-commands=no --daemon=no
 while ! nc -z localhost $PORT ; do
     sleep 0.1
 done
