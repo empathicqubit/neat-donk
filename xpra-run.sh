b5f3084ddcb72bb79a2341d486e46df915502be5
empathicqubit <empathicqubit@entan.gl>
2021-05-01 22:16:45 -0400

    Run the check for Xpra in the background

--- xpra-run.sh
+++ xpra-run.sh
@@ -3,10 +3,12 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 #ARGS=("--lua=$SCRIPT_DIR/neat-donk.lua")
 ARGS=()
 PORT=5309
+(
+    while ! nc -z localhost $PORT ; do
+        sleep 0.1
+    done
+    xdg-open http://127.0.0.1:$PORT
+) &
 xpra start --bind-tcp=127.0.0.1:$PORT --html=on \
     --start-child="/etc/alternatives/x-terminal-emulator -e 'lsnes ${ARGS[*]}'" \
     --exit-with-child=yes --start-new-commands=no --daemon=no
-while ! nc -z localhost $PORT ; do
-    sleep 0.1
-done
-xdg-open http://127.0.0.1:$PORT
